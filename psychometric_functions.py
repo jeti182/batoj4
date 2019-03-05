c1d124dff0c22de3ae2be17ffa3a81d9c3708a7b
jeti182 <jeti@mail.upb.de>
2019-03-05 13:06:29 +0100

    Update psychometric_functions.py

--- psychometric_functions.py
+++ psychometric_functions.py
@@ -2,4 +2,13 @@ import tensorflow as tf
 
 
 def logistic_pss_dl(soas,pss,dl):
+ """Logistic psychometric function. Parametrized to directly take
+the PSS and DL as arguments. 
+
+:param soas: Stimulus Onset Asynchronies
+:type soas: List-like object with float-like numbers of the SOAs in ms.
+
+:return: List with the probability of judging `probe first` at each SOA
+:rtype: Tensor
+"""
  return (1.0-(1.0/(1.0+tf.exp(-(1.0/dl*(soas-pss))))))
