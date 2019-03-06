71d6945703de8a3d0d2bca26d813e230231e1043
jeti182 <jan.tuennermann@uni-marburg.de>
2019-03-06 12:02:38 +0100

    Definition of a common model interface. A model knows its psychometric function and how to simulate itself. Moreover, it provides descriptions of its parameters and references where it was used in the scientific literature. Hence, a TOJModel is representation of the logical/mathematical/scientific model and does not include a statistical model yet.

---
+++ models/logistic_pss_dl_model.py
@@ -0,0 +1,35 @@
+import abc
+from toj_model import TOJModel, ParameterDescription
+
+class LogisticPSSDLModel(TOJModel):
+	
+	parameters_descriptions = [ 
+			ParameterDescription("PSS", "Point of Subjective Simultaneity",
+			"the point where probe-first function corsses the .5 level"),
+			ParameterDescription("DL", "Difference",
+			"TODO")]
+	
+	used_by = ["TODO", "TODO"]
+
+	def psychometric_function(self, soas, parameters):
+		""" Function of judging `probe first` over the SOAs """
+		(pss, dl) = parameters
+		return (1.0-(1.0/(1.0+tf.exp(-(1.0/dl*(soas-pss))))))
+
+	def summary(self): # TODO: This impl. can go into super class.
+		""" Provide a summary description of this model, possibly using
+		the information properties """
+		pd = self.parameters_descriptions
+		return(" A traditional psychometric model with parameters " + 
+		pd[0].symbol + " (" + pd[0].full_name + ") " + "which is " +
+		pd[0].meaning + 
+		" and " +
+		pd[1].symbol + " (" + pd[1].full_name + ") " + "which is " +
+		pd[1].meaning) 
+
+	def simulate(self, parameters):
+		print("Not implemented yet!")	
+
+if __name__ == '__main__':
+	model = LogisticPSSDLModel()
+	print(model.summary())
---
+++ models/toj_model.py
@@ -0,0 +1,43 @@
+import abc
+
+class ParameterDescription():
+	""" Struct for parameter descriptions """
+	def __init__(self, symbol="x", full_name="unnamed",
+				meaning="This parameter has no meaning defined "):
+		self.symbol = symbol
+		self.symbol_math_font = symbol
+		self.full_name = full_name
+		self.meaning = meaning
+
+class TOJModel(abc.ABC):
+	""" This is the base class for TOJ models """
+	@abc.abstractmethod
+	def psychometric_function(self, soas, parameters):
+		""" Function of judging `probe first` over the SOAs """
+		return
+		
+	@abc.abstractmethod
+	def simulate(self, parameters):
+		""" Implement a (if possible generative) simulation """
+		return
+        
+	@abc.abstractmethod
+	def summary(self):
+		""" Provide a summary description of this model, possibly using
+		the information properties """
+		return	
+
+	@property
+	@abc.abstractmethod
+	def parameters_descriptions(self):
+		""" Provide list of parameter descriptions """
+		pass	
+	
+	@property
+	@abc.abstractmethod
+	def used_by(self):
+		""" Provide list of references """
+		pass
+		
+
+		
