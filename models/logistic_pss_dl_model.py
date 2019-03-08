e4a5af541e96d33d5c315bca4af15ebaca503f26
jeti182 <jan.tuennermann@uni-marburg.de>
2019-03-09 00:01:28 +0100

    Further work on generic simulation

--- models/logistic_pss_dl_model.py
+++ models/logistic_pss_dl_model.py
@@ -1,12 +1,16 @@
+import abc
 from toj_model import TOJModel, ParameterDescription
-
+import tensorflow as tf
+import numpy
 
 class LogisticPSSDLModel(TOJModel):
 	
+	model_description = "Traditional psychometric sigmoid. "
+	
 	parameters_descriptions = [ 
 			ParameterDescription("PSS", "Point of Subjective Simultaneity",
-			"the point where probe-first function corsses the .5 level"),
-			ParameterDescription("DL", "Difference",
+			"The point where probe-first function corsses the .5 level"),
+			ParameterDescription("DL", "Difference Limen",
 			"TODO")]
 	
 	used_by = ["TODO", "TODO"]
@@ -16,21 +20,20 @@ class LogisticPSSDLModel(TOJModel):
 		(pss, dl) = parameters
 		return (1.0-(1.0/(1.0+tf.exp(-(1.0/dl*(soas-pss))))))
 
-	def summary(self): # TODO: This impl. can go into super class.
-		""" Provide a summary description of this model, possibly using
-		the information properties """
-		pd = self.parameters_descriptions
-		return(" A traditional psychometric model with parameters " +
-			   pd[0].symbol + " (" + pd[0].full_name + ") " + "which is " +
-			   pd[0].meaning +
-		" and " +
-			   pd[1].symbol + " (" + pd[1].full_name + ") " + 'which is ' +
-			   pd[1].meaning)
-
-	def simulate(self, parameters):
-		print("Not implemented yet!")	
-
+###	def simulate(self, parameters):
+###		print("Not implemented yet!")	
 
 if __name__ == '__main__':
 	model = LogisticPSSDLModel()
 	print(model.summary())
+	soas = numpy.linspace(-100.0,100.0,21)
+	repetitions = [32] * 21
+	pse = 10
+	dl = 30
+	print("Simulating myself for SOAs = " + str(soas) +
+			" and Repetitions = " + str(repetitions) + 
+			" with paramters PSE = " + str(pse) +
+			" and DL = " + str(dl))
+	tojs = model.simulate(soas, repetitions, (pse, dl))	
+	print(tojs)
+	
--- models/toj_model.py
+++ models/toj_model.py
@@ -1,15 +1,15 @@
 import abc
+from workarounds import binomial  
+import pandas as pd
 
-
-class ParameterDescription:
+class ParameterDescription():
 	""" Struct for parameter descriptions """
 	def __init__(self, symbol="x", full_name="unnamed",
-					meaning="This parameter has no meaning defined "):
+				meaning="This parameter has no meaning defined "):
 		self.symbol = symbol
 		self.symbol_math_font = symbol
 		self.full_name = full_name
 		self.meaning = meaning
-
 
 class TOJModel(abc.ABC):
 	""" This is the base class for TOJ models """
@@ -18,16 +18,42 @@ class TOJModel(abc.ABC):
 		""" Function of judging `probe first` over the SOAs """
 		return
 		
-	@abc.abstractmethod
-	def simulate(self, parameters):
-		""" Implement a (if possible generative) simulation """
-		return
-
-	@abc.abstractmethod
+	def simulate(self, soas, repetitions, parameters, 
+				prameter_distributions = None, number_of_subjects = 1):
+		""" A default simulation is implemented, which uses the models
+		psychometric function to simulate binomially distributed data 
+		ponits. How ever, model developers are encouraged to reimpliment
+		the stimulation (by overriding this function) in a more
+		instructive genrative, mechanistic form, if their model allows
+		this. """
+		list_of_tojs = []
+		for i in range(number_of_subjects):
+			individual_tojs = pd.DataFrame()
+			thetas = self.psychometric_function(soas, parameters)
+			individual_tojs['Subject'] = [i+1]*len(soas)
+			individual_tojs['Codition'] = [1]*len(soas)
+			individual_tojs['SOA'] = soas  
+			individual_tojs['Repetitions'] = repetitions  
+			individual_tojs['Probe_first_count'] = binomial(repetitions, thetas)
+			list_of_tojs.append(individual_tojs)
+			tojs = pd.concat(list_of_tojs) 
+		return tojs
+        
 	def summary(self):
 		""" Provide a summary description of this model, possibly using
 		the information properties """
-		return	
+		pd = self.parameters_descriptions
+		summary = self.model_description + "\n Parameters: \n" 
+		for i in range(len(pd)):
+			summary +=  "*" + (pd[i].symbol + " (" + pd[i].full_name + "): " + 
+					 pd[i].meaning + "\n") 
+		return(summary)
+		
+	@property
+	@abc.abstractmethod
+	def model_description(self):
+		""" Profide a brief description """
+		pass
 
 	@property
 	@abc.abstractmethod
@@ -40,3 +66,8 @@ class TOJModel(abc.ABC):
 	def used_by(self):
 		""" Provide list of references """
 		pass
+		
+
+		
+
+		
